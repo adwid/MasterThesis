63669bea26d592d504628d94b203c40a258f04c7
adwid <adrien.widart@student.uclouvain.be>
2020-06-13 23:43:00 +0200

    run all tests

---
+++ test_all_modules.js
@@ -0,0 +1,17 @@
+process.env["ENV_FILE_PATH"] = __dirname + "/.env";
+
+describe('TEST ALL MODULES', function () {
+
+    it('should test the Agenda Module', function () {
+        require("./Agenda/Test/index");
+    });
+
+    it('should test the Carpooling Module', function () {
+        require("./Carpooling/Test/index");
+    });
+
+    it('should test the Rental Module', function () {
+        require("./Rental/Test/index");
+    })
+
+});
